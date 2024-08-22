5e0d21704c01d29206b8bf049b2442bd306784e8
Adil Aman Mohammed <114213304+adilamanmohammed@users.noreply.github.com>
2024-08-21 22:57:01 -0400

    Random Jokes App Project Created

---
+++ Jokes_App/Jokes_App/Jokes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_AppApp.swift
+//  Jokes_App
+//
+//  Created by Adil Aman Mohammed on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
